c9bfc890d4ada1a5304a5042b5b8743da77a2af0
dagnaldo <naldo.penha@gmail.com>
2018-05-10 18:24:24 -0300

    improving tests for scene_info with scene_id and adding pre_collection test data

--- tests/test_landsat_scene_info.py
+++ tests/test_landsat_scene_info.py
@@ -5,16 +5,19 @@
 
 from datetime import datetime
 
-from landsat_downloader.scene_info import SceneInfo
+from landsat_downloader.scene_info import SceneInfo, Info
 
 
 def test_scene_info_product_rt():
-    scene = "LC08_L1GT_224069_20180206_20180301_01_T1"
+    scene_id = "LC82240692018037LGN00"
+    scene_t2 = "LC08_L1GT_224069_20180206_20180221_01_T2"
     scene_rt = "LC08_L1GT_224069_20180206_20180206_01_RT"
 
-    scene_info = SceneInfo(product_id=scene)
-    product = scene_info.make_rt_product_id(scene)
+    scene_info = SceneInfo(scene_id=scene_id)
+    product = scene_info.make_rt_product_id(scene_t2)
+    assert(product == scene_rt)
 
+    product = scene_info.make_rt_product_id()
     assert(product == scene_rt)
 
 
@@ -24,13 +27,12 @@ def test_scene_info_product():
     scene_rt = "LC08_L1GT_224068_20180310_20180310_01_RT"
 
     scene_info = SceneInfo(scene_id=scene)
-    info = scene_info.get_info_from_scene_id(scene)
-    assert(info.get('path') == 224)
-    assert(info.get('row') == 68)
-    assert(info.get('year') == 2018)
-    assert(info.get('gsi') == "LGN")
-    assert(info.get('version') == "00")
-    assert(info.get('acq_date') == datetime(2018, 3, 10).date())
+    assert(scene_info.info.path == 224)
+    assert(scene_info.info.row == 68)
+    assert(scene_info.info.year == 2018)
+    assert(scene_info.info.gsi == "LGN")
+    assert(scene_info.info.version == "00")
+    assert(scene_info.info.acq_date == datetime(2018, 3, 10).date())
 
     assert(scene_info.product_id != "{}.TIF".format(scene_t2))
     assert(scene_info.product_id == scene_t2)
@@ -40,9 +42,20 @@ def test_scene_info_product():
 
 def test_scene_info_julian_2_date():
     scene = "LC82240682018069LGN00"
-    scene_info = SceneInfo(scene_id=scene)
+    scene_info = Info(scene_id=scene)
     date = scene_info.julian_2_date("2015070")
     assert(date == datetime(2015, 3, 11).date())
 
     date = scene_info.julian_2_date("2018069")
     assert(date == datetime(2018, 3, 10).date())
+
+    date = scene_info.julian_2_date("2018001")
+    assert(date == datetime(2018, 1, 1).date())
+
+
+def test_scene_info_product_loop():
+    scene = "LC82240682018069LGN0"
+    for i in range(2, 5):
+        s = "{}{}".format(scene, i)
+        scene_info = SceneInfo(scene_id=s, update_scene=True)
+        assert(scene_info.scene_id == "LC82240682018069LGN00")
